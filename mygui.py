59831fa0aa6b4c548e96b377cea64283d2b73f38
Aragya1642 <56979762+Aragya1642@users.noreply.github.com>
2023-03-16 15:42:51 -0400

    Image upload and cropping w/ resizing

--- mygui.py
+++ mygui.py
@@ -2,6 +2,12 @@
 from tkinter import *
 import tkinter as tk  # This library allows for the development of a GUI
 from stringart import StringArtGenerator  # This is the string art generator from previous students
+import os # Allows for the interaction with the computer os (I think)
+from tkinter import filedialog # For Upload function
+from PIL import ImageTk, Image # For Upload function
+
+
+image_list = {}
 
 # Trying to do this with OOP
 class OptimizationGUI:
@@ -67,6 +73,23 @@ class OptimizationGUI:
 
         self.entry5 = tk.Entry(self.line_frame)
         self.entry5.pack(padx=15, side=tk.LEFT)
+        ########################################## - Upload
+        self.sublabel5 = tk.Label(self.root, text="Upload your Image", font=('Arial', 14))
+        self.sublabel5.pack(padx=10, pady=10, anchor="nw")
+
+        self.button1 = tk.Button(self.root, text="Upload", font=('Arial', 10), command= self.Upload)
+        self.button1.pack(padx=10, pady=0, anchor="nw")
+        ########################################## - Shows Image path
+        self.path_label = tk.Label(self.root, text = '  ')
+        self.path_label.pack()
+        img_path_value = self.path_label.cget("text")
+        ########################################## - Showing the uploaded image
+        self.imageLabel = tk.Label(self.root)
+        self.imageLabel.pack(padx=10,pady=10)
+        ########################################## - Start generating the stringart
+        self.generate_btn = Button(self.root, text = "Optimize my Picture", font= ('Arial', 30))
+        self.generate_btn.pack(side=tk.TOP)
+        # Make sure to add a command to the optimize button
         ##########################################
         self.root.mainloop()
 
@@ -77,5 +100,35 @@ class OptimizationGUI:
         elif self.check2_state.get() == 1:
             self.check1_state.set(0)
 
+    def Upload(self):
+        print('upload')
+        selectFileName = filedialog.askopenfile(mode='r', title = 'Select a image', filetypes=[
+                        ("image", ".jpeg"),
+                        ("image", ".png"),
+                        ("image", ".jpg"),
+                    ])
+        filepath = os.path.abspath(selectFileName.name)
+        print(filepath)
+        self.path_label.configure(text=filepath)
+
+        # Create an object of tkinter ImageTk
+        img = Image.open(self.path_label.cget("text"))
+            # Resize the image
+        orig_width, orig_height = img.size
+        scale_factor = max(orig_height, orig_width) / 200
+        resized_img = img.resize((int(orig_width/scale_factor), int(orig_height/scale_factor)))
+        white_img = Image.new('RGB', (200, 200), (255, 255, 255))
+        resized_width, resized_height = resized_img.size
+        paste_x = int((200 - resized_width) / 2)
+        paste_y = int((200 - resized_height) / 2)
+        white_img.paste(resized_img, (paste_x, paste_y))
+        upload_img_tk = ImageTk.PhotoImage(white_img)
+        image_list['upload_img'] = upload_img_tk
+
+        # Create a Label Widget to display the text or Image
+        self.imageLabel.configure(image = image_list.get('upload_img'))
+        self.imageLabel.image = image_list.get('upload_img')
+        return filepath
+    
 # This is where code starts and refrences back to my class
 OptimizationGUI()
